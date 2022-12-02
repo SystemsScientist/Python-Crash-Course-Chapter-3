3924e7683d4f1f9f2e3e5dad04ce382a7bb00c75
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-02 06:50:17 -0600

    Fourth update for bicycles.py file

--- Examples/bicycles.py
+++ Examples/bicycles.py
@@ -6,8 +6,10 @@ bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
 print(bicycles[0])
 print(bicycles[0].title())
+
 print(bicycles[1])
 print(bicycles[2])
 
+print(bicycles[-1])
 
 
