16455b9da6a11f66ffb10f82c27871b9eb0644a2
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-03 06:48:10 -0600

    Adding exercise_03_01.py file

---
+++ Exercises/exercise_03_01.py
@@ -0,0 +1,16 @@
+# exercise_03_01.py, Chapter 03, Python Crash Course
+#
+# Names: store the names of a few of your friends in
+# a list called names. Print each person's name by
+# accessing each element in the list, on at a time.
+
+names = ['alan', 'dennis', 'ken', 'ada', 'brian']
+
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+print(names[4].title())
+
+
+
