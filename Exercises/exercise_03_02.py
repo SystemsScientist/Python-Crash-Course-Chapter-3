45efc394d5388872a46a192d86b2ab5851bcbc03
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-04 06:32:45 -0600

    Adding exercise_03_02.py file

---
+++ Exercises/exercise_03_02.py
@@ -0,0 +1,18 @@
+# exercise_03_02.py, Chapter 03, Python Crash Course
+# 
+# Greetings: start with the list you used in Exercise 3-1,
+# but instead of just printing each person's name, print
+# a message to them. The text of each message should be
+# the same, but each message should be personalized with
+# the person's name
+
+names = ['alan', 'dennis', 'ken', 'ada', 'brian'];
+
+print("Hello, " + names[0].title() + ". It is nice to meet you!")
+print("Hello, " + names[1].title() + ". It is nice to meet you!")
+print("Hello, " + names[2].title() + ". It is nice to meet you!")
+print("Hello, " + names[3].title() + ". It is nice to meet you!")
+print("Hello, " + names[4].title() + ". It is nice to meet you!")
+
+
+
