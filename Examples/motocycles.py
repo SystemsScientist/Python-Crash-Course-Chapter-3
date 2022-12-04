d5ec279ccfe345c2deb9975b153e4d86bdf2d202
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-04 06:49:02 -0600

    Adding motorcycles.py file

---
+++ Examples/motocycles.py
@@ -0,0 +1,11 @@
+# motocycles.py, Chapter 03, Python Crash Course
+# program prints list of motorcycles
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+
+
