a188d1bcdb84cd752e4271d252a13dd94c9daaf9
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-04 06:43:02 -0600

    Adding exercise_03_03.py file

---
+++ Exercises/exercise_03_03.py
@@ -0,0 +1,16 @@
+# exercise_03_03.py, Chapter 03, Python Crash Course
+# 
+# Your Own List: Think of your favorite mode of transportation,
+# such as a motorcycle or a car, and make a list that stores
+# several examples. Use your list to print a series of statements
+# about these items, such as "I would like to own a Honda
+# motorcycle."
+
+list = ['2022 dodge durango', '1967 Chevy Impala 4-Door', '2022 Dodge Challenger']
+
+print("I would like to own a " + list[0].title() + "!")
+print("I would like to own a " + list[1].title() + "!")
+print("I would like to own a " + list[2].title() + "!")
+
+
+
