3fb382f3c45d9e91a28097040efe6d4888ef3c9a
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-04 07:03:49 -0600

    Adding motorcycles3.py file

---
+++ Examples/motorcycles3.py
@@ -0,0 +1,13 @@
+# motorcycles3.py, Chapter 03, Python Crash Course
+# program appends motorcycles to an empty list
+
+motorcycles = []
+
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+
+print(motorcycles)
+
+
+
