5b84d4211ac7c006d1a1594f17ab830dbc0f18aa
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-05 06:55:30 -0600

    Adding motorcycles6.py file

---
+++ Examples/motorcycles6.py
@@ -0,0 +1,14 @@
+# motorcycles6.py, Chapter 03, Python Crash Course
+# program pops elements from a list using the pop()
+# method
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+
+
