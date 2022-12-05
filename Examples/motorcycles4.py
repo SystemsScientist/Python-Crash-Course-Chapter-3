f7d7cd20bdfb79b4fa799f18597a771b9af99801
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-05 06:37:00 -0600

    Adding motorcycles4.py file

---
+++ Examples/motorcycles4.py
@@ -0,0 +1,11 @@
+# motorcycles4.py, Chapter 03, Python Crash Course
+# program adds a new element to the list via the
+# insert method
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+
+
