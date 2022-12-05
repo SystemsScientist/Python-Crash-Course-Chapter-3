e4ca2d856f5146c798c0e0ca3b0cc24dec9428fc
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-05 06:48:59 -0600

    Adding motorcycles5.py file

---
+++ Examples/motorcycles5.py
@@ -0,0 +1,24 @@
+# motorcycles5.py, Chapter 03, Python Crash Course
+# program deletes an element from the list by using
+# 'del'
+#
+# Questions:
+# 
+# what is the output when the element at position 
+# '0' is deleted the first time?
+#
+# what is the output when the element at position
+# '0' is deleted the second time?
+
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+del motorcycles[0] # first delete
+print(motorcycles)
+
+del motorcycles[0] # second delete
+print(motorcycles)
+
+
+
