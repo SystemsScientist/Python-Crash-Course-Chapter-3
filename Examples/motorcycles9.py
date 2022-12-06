a4fe76260351d8543b05bf4d92f502fb874112fc
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-06 06:52:20 -0600

    Adding motorcycles9.py file

---
+++ Examples/motorcycles9.py
@@ -0,0 +1,12 @@
+# motorcycles9.py, Chapter 03, Python Crash Course
+# program prints a list, removes an item from a list,
+# and then prints the list again
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+motorcycles.remove('ducati')
+print(motorcycles)
+
+
+
