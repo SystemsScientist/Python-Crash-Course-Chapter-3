a746abf70efdc29c0e6545d5fc4949e00df47721
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-06 06:37:53 -0600

    Adding motorcycles7.py file

---
+++ Examples/motorcycles7.py
@@ -0,0 +1,11 @@
+# motorcycles7.py, Chapter 03, Python Crash Course
+# program pops elements from a list using the pop()
+# method and then prints the last element
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+last_owned = motorcycles.pop()
+print("The last motorcycle I owned was a " + last_owned.title() + ".")
+
+
+
