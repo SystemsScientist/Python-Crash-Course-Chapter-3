ace95cde7b70dcdd64629e58345df3e95dd9b762
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-06 06:46:31 -0600

    Adding motorcycles8.py file

---
+++ Examples/motorcycles8.py
@@ -0,0 +1,11 @@
+# motorcycles8.py, Chapter 03, Python Crash Course
+# program pops an element from a list using the pop()
+# method and then prints that element
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a " + first_owned.title() + ".")
+
+
+
