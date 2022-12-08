ca2a3ca7d5082732b08e59ad5097595d258bd3f0
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-08 08:46:20 -0600

    Adding cars.py file

---
+++ Examples/cars.py
@@ -0,0 +1,10 @@
+# cars.py, Chapter 03, Python Crash Course
+# program sorts and then prints a list of cars
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+cars.sort() # sort method
+print(cars)
+
+
+
