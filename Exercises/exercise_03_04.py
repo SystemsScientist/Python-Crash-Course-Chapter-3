5cd7bf1be84e9a5b8ec2480586a3605e4ab67f4a
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-07 20:24:53 -0600

    Adding exercise_03_04.py file

---
+++ Exercises/exercise_03_04.py
@@ -0,0 +1,21 @@
+# exercise_03_04.py, Chapter 03, Python Crash Course
+#
+# Guest List: If you could invite anyone, living or deceased,
+# to dinner, who would you invite? Make a list that includes
+# at least three people you'd like to invite to dinner. Then
+# use your list to print a message to each person, inviting
+# them to dinner.
+
+guest_list = ['benjamin franklin', 'frederick douglass', 
+              'samuel clemens', 'alan turing', 'gene roddenberry', 
+              'mel brooks']
+
+print("Dear, Mr. " + guest_list[0].title() + "! You are cordially invited to The Dinner of the Minds.")
+print("Dear, Mr. " + guest_list[1].title() + "! You are cordially invited to The Dinner of the Minds.")
+print("Dear, Mr. " + guest_list[2].title() + "! You are cordially invited to The Dinner of the Minds.")
+print("Dear, Dr. " + guest_list[3].title() + "! You are cordially invited to The Dinner of the Minds.")
+print("Dear, Mr. " + guest_list[4].title() + "! You are cordially invited to The Dinner of the Minds.")
+print("Dear, Mr. " + guest_list[5].title() + "! You are cordially invited to The Dinner of the Minds.")
+
+
+
