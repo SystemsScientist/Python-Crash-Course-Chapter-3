8b9fbcb4704d1c9b2dedf5cb07d0b12a487292e8
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-09 06:47:33 -0600

    Adding cars2.py file

---
+++ Examples/cars2.py
@@ -0,0 +1,11 @@
+# cars2.py, Chapter 03, Python Crash Course
+# program sorts a list of cars in reverse and then
+# prints the list of cars
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+cars.sort(reverse=True)
+print(cars)
+
+
+
