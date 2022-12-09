27afdbad138460a2117be3a072592276bc81db07
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-09 06:53:29 -0600

    Adding cars3.py file

---
+++ Examples/cars3.py
@@ -0,0 +1,18 @@
+# cars3.py, Chapter 03, Python Crash Course
+# program prints a list of cars, sorts a list
+# of cars using sorted, and then prints the list
+# of cars
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list: ")
+print(cars)
+
+print("\nHere is the sorted list: ")
+print(sorted(cars))
+
+print("\nHere is the original list again: ")
+print(cars)
+
+
+
