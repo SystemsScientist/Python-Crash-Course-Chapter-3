157244f42b28d708e24dec60b47f3562998e9201
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-10 06:07:28 -0600

    Adding cars4.py file

---
+++ Examples/cars4.py
@@ -0,0 +1,13 @@
+# cars4.py, Chapter 03, Python Crash Course
+# program prints a list of cars, uses the
+# reverse method, and then prints the list
+# of cars
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+
+cars.reverse()
+print(cars)
+
+
+
