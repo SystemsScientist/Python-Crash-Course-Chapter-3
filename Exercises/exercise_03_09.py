07e845bae5830c03900fb636f86194f33afb68d9
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-19 18:53:46 -0600

    Adding exercise_03_09.py file

---
+++ Exercises/exercise_03_09.py
@@ -0,0 +1,16 @@
+# exercise_03_09.py, Chapter 03, Python Crash Course
+#
+# Dinner Guests: Working with one of the programs from Exercises 3-4
+# through 3-7 (page 46), use len() to print a message indicating the
+# number of people you are inviting to dinner.
+
+guest_list = ['benjamin franklin', 'frederick douglass', 
+              'samuel clemens', 'alan turing', 
+              'thomas sowell', 'gene roddenberry', 
+              'morgan freeman', 'mel brooks']
+
+list_length = len(guest_list)
+print("The Dinner of the Minds will be hosting " + str(list_length) + " guests this evening.")
+
+
+
